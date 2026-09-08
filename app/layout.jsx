7499036327b79ba68c946a889a1c819969c1db58
Nikunj Hapani <nikunj.hapani7035@gmail.com>
2026-09-08 17:33:28 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import ConditionalLayout from "../components/ConditionalLayout";
+import Script from "next/script";
 
 export const metadata = {
   title: "Anjani Industries",
   description: "Anjani Industries",
-
   icons: {
-    icon: "../assets/img/favicon.png",
-    shortcut: "../assets/img/favicon.png",
-    apple: "../assets/img/favicon.png",
+    icon: "/assets/img/favicon.png",
+    shortcut: "/assets/img/favicon.png",
+    apple: "/assets/img/favicon.png",
   },
 };
 
@@ -16,9 +16,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <ConditionalLayout>
-          {children}
-        </ConditionalLayout>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NW6Z613EES"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NW6Z613EES');
+          `}
+        </Script>
+
+        <ConditionalLayout>{children}</ConditionalLayout>
       </body>
     </html>
   );
